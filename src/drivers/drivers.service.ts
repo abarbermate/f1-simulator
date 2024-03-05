07f2247820a528dda4cf7cd0cc6d75492e7a7252
Barbér Máté <mate.barber@kifli.tech>
2024-03-05 23:30:26 +0100

    Create DriversService with overtake method

---
+++ src/drivers/drivers.service.ts
@@ -0,0 +1,42 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { DataService } from '../data/data.service';
+
+@Injectable()
+export class DriversService {
+  constructor(private dataService: DataService) {}
+  overtake(driverId: string) {
+    const drivers = this.dataService.getData();
+    const driverIndex = drivers.findIndex(
+      (driver) => driver.id === parseInt(driverId),
+    );
+
+    if (driverIndex === -1) {
+      throw new NotFoundException(`Driver #${driverId} not found.`);
+    }
+
+    const driverPlace = drivers[driverIndex].place;
+
+    if (!driverPlace || driverPlace === 1) {
+      throw new BadRequestException(`Driver #${driverId} can't overtake.`);
+    }
+
+    const prevDriverIndex = drivers.findIndex(
+      (driver) => driver.place === driverPlace - 1,
+    );
+
+    if (prevDriverIndex === -1) {
+      console.error('Previous driver not found.');
+      throw new InternalServerErrorException();
+    }
+
+    const prevDriverPlace = drivers[prevDriverIndex].place;
+
+    this.dataService.patchData(driverIndex, { place: prevDriverPlace });
+    this.dataService.patchData(prevDriverIndex, { place: driverPlace });
+  }
+}
