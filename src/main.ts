49fc5b1e6013b7a904bc48bbe9679fbbe4afb206
Barbér Máté <mate.barber@kifli.tech>
2024-03-06 00:50:03 +0100

    Fix starting on a port from dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   const url = await app.getUrl();
